9a2a19535b321a3638c02d5c6f4c17d0f64e41bb
esraa <ezraaelhemaily@gmail.com>
2025-12-01 20:53:26 +0200

    Added new updates

---
+++ database/migrations/2025_12_01_181657_create_cameras_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cameras', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null'); // Foreign Key to users table (optional assignment)
+            $table->string('location');
+            $table->string('status');
+            $table->date('installation_date');
+            $table->string('department');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cameras');
+    }
+};
