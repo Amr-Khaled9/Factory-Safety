1e65cd5e9cd0c24e2e12284261047355e863d951
AmrKhaled9 <amrkhaled500@gmail.com>
2026-06-18 15:42:13 +0300

    Fix undefined array key in realtime notifications and improve safe data access

--- resources/views/livewire/realtime-danger-alert.blade.php
+++ resources/views/livewire/realtime-danger-alert.blade.php
@@ -2,31 +2,31 @@
 
     @foreach($notifications as $notification)
 
+    @php
+        $peeId = $notification->data['pee_log_id'] ?? null;
+        $vehicleId = $notification->data['vehicle_log_id'] ?? null;
+    @endphp
+
     <div
         class="danger-alert"
-
         x-data="{ show: true }"
-
-        x-init="
-                setTimeout(() => show = false, 5000)
-            "
-
+        x-init="setTimeout(() => show = false, 5000)"
         x-show="show"
 
         @click="
-                window.location.href =
-                '{{ isset($notification->data['pee_log_id'])
-                    ? '/detections/'.$notification->data['pee_log_id']
-                    : '/gate/'.$notification->data['vehicle_log_id']
-                }}'
-            ">
+            window.location.href =
+            '{{ $peeId
+                ? '/detections/'.$peeId
+                : ($vehicleId ? '/gate/'.$vehicleId : '/gate')
+            }}'
+        ">
 
         <strong>
-            {{ $notification->data['title'] }}
+            {{ $notification->data['title'] ?? 'No title' }}
         </strong>
 
         <p>
-            {{ $notification->data['message'] }}
+            {{ $notification->data['message'] ?? '' }}
         </p>
 
     </div>
